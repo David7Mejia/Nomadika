f64675f91f5b3c17c39e9c26771381bae278c0bc
David Mejia <david7mejia@gmail.com>
2021-07-31 15:15:42 -0500

    favicon

--- app/__init__.py
+++ app/__init__.py
@@ -69,5 +69,5 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+        return app.send_static_file('favicon.svg')
     return app.send_static_file('index.html')
