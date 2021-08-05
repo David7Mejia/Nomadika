653e1f121428861f4a4510832f2057f0dea440ad
David Mejia <david7mejia@gmail.com>
2021-08-05 12:22:57 -0500

    tried fixing issue

--- app/api/city_routes.py
+++ app/api/city_routes.py
@@ -6,10 +6,11 @@ city_routes = Blueprint('cities', __name__)
 
 @city_routes.route('/<int:id>')
 def get_dest_feeds(id):
-    feed = Feed.query.filter_by(loc_id=str(id)).first()
+    pass
+    feed = Feed.query.filter_by(loc_id=str(id)).all()
     print('###################################', feed)
-    return feed.to_dict()
-    # return [fd.to_dict() for fd in feed]
+    # return feed.to_dict()
+    return [fd.to_dict() for fd in feed]
 
 
 @city_routes.route('/<int:id>', methods=['POST'])
@@ -26,17 +27,27 @@ def post_feed(id):
 
 
 @city_routes.route('/<int:id>/location', methods=['POST'])
-def post_location(id=None):
+def post_location(id):
     req = request.get_json()
-    loc = Location(
-        api_id=req['api_id'],
-        name=req['name'],
-        image_url=req['image_url'],
-        description=req['description']
-    )
-    db.session.add(loc)
-    db.session.commit()
-    return loc.to_dict()
+    locExists = Location.query(api_id=id).exists()
+    exists = db.session.query(Location.api_id).filter_by(api_id=id).first() is not None
+
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', exists)
+    pass
+    # if locExists:
+    #     single_loc = Location.query.get(api_id=id).first()
+    #     return single_loc.to_dict()
+    # else:
+    #     loc = Location(
+    #         api_id=req['api_id'],
+    #         name=req['name'],
+    #         image_url=req['image_url'],
+    #         description=req['description']
+    #     )
+
+    #     db.session.add(loc)
+    #     db.session.commit()
+    #     return loc.to_dict()
 
 
 @city_routes.route('/<int:id>/<desc>', methods=['PUT'])
