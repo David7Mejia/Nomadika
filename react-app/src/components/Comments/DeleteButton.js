f127a58138e6e527fb083f1cd2735257f638b193
David Mejia <david7mejia@gmail.com>
2021-08-08 17:50:25 -0500

    final

--- react-app/src/components/Comments/DeleteButton.js
+++ react-app/src/components/Comments/DeleteButton.js
@@ -1,30 +1,30 @@
-import React from "react";
-import {  useDispatch } from "react-redux";
-import { deleteComment } from "../../store/comment";
-import { getDestFeed } from "../../store/destination";
-import "./Comments.css";
+// import React from "react";
+// import {  useDispatch } from "react-redux";
+// import { deleteComment } from "../../store/comment";
+// import { getDestFeed } from "../../store/destination";
+// import "./Comments.css";
 
-function DeleteButton({ feed, comment }) {
-  const dispatch = useDispatch();
+// function DeleteButton({ feed, comment }) {
+//   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  // }, [dispatch, feed.loc_id]);
+//   // useEffect(() => {
+//   // }, [dispatch, feed.loc_id]);
 
-  const deleteHandler = (id) => {
-    dispatch(deleteComment(id));
-    dispatch(getDestFeed(feed.loc_id));
-  };
+//   const deleteHandler = (id) => {
+//     dispatch(deleteComment(id));
+//     dispatch(getDestFeed(feed.loc_id));
+//   };
 
-  useEffect(() => {}, []);
-  return (
-    <div>
-      <button
-        className="cmt-delete-button"
-        onClick={() => deleteHandler(comment.id)}
-      >
-      </button>
-    </div>
-  );
-}
+//   useEffect(() => {}, []);
+//   return (
+//     <div>
+//       <button
+//         className="cmt-delete-button"
+//         onClick={() => deleteHandler(comment.id)}
+//       >
+//       </button>
+//     </div>
+//   );
+// }
 
-export default DeleteButton;
+// export default DeleteButton;
