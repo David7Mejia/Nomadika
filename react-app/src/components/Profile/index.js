c4d7bc5fbaa7ba79231c3682598c292fd523f5b9
David Mejia <david7mejia@gmail.com>
2021-09-10 16:57:27 -0500

    delete venues from profile

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,6 +1,6 @@
 import {useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import {getUserVenuesThunk} from '../../store/gotos'
+import { getUserVenuesThunk, deleteGotoVenueThunk } from "../../store/gotos";
 import "./Profile.css";
 
 function Profile() {
@@ -9,8 +9,8 @@ function Profile() {
   const userPlaces = useSelector((state) => Object.values(state?.gotos));
   const baseURL = `https://www.google.com/maps/search/?api=1&query=`;
 
-  const googleMapUrl = (loc, ven) => {
-    let str = `${loc} ${ven}`;
+  const googleMapUrl = (loc, ven, address) => {
+    let str = `${loc} ${ven} ${address}`;
     let newURL = str.split(" ").join("+");
     return `${baseURL}${newURL}`;
   };
@@ -19,14 +19,23 @@ function Profile() {
     let obj = {};
     place[0]?.forEach((item) => {
       if (obj[item.loc_name]) {
-        obj[item.loc_name].push(`${item.venue_name}, ${item.address}`);
+        obj[item.loc_name].push(
+        [item.venue_name, item.address, item.id]
+        );
       } else {
-        obj[item.loc_name] = [`${item.venue_name}, ${item.address}`];
+        obj[item.loc_name] = [[item.venue_name, item.address, item.id]];
       }
     });
     return obj;
   };
+
   let userVenues = placeConverter(userPlaces);
+
+   const deleteVenue = async (id) => {
+    await dispatch(deleteGotoVenueThunk(id));
+    await dispatch(getUserVenuesThunk(sessionUser.id));
+
+   };
 
   useEffect(() => {
     dispatch(getUserVenuesThunk(sessionUser.id));
@@ -39,17 +48,24 @@ function Profile() {
         {userVenues &&
           Object.entries(userVenues).map(([key, value]) => (
             <div className="profile-venue-city">
+              {console.log(userVenues)}
               <p className="profile-city-name">{key}</p>
               <div className="places-holder" id="profile-places-holder">
                 {value.map((item) => (
                   <div className="venue" id="places-holder">
+                    {console.log("this is the item", item)}
                     <a
                       rel="noreferrer"
-                      href={googleMapUrl(key, item)}
+                      href={googleMapUrl(key, item[0], item[1])}
                       target="_blank"
                     >
-                      {item}
+                      {item[0]}
+                      {item[1]}
                     </a>
+                    <button
+                      id="delete-venue"
+                      onClick={() => deleteVenue(item[2])}
+                    ></button>
                   </div>
                 ))}
               </div>
